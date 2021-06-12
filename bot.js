7e910aa80b5ecd6a4841a07490c62f1b0707bdd3
burturt <burturt>
2021-06-11 21:57:26 -0700

    Add help command

--- bot.js
+++ bot.js
@@ -55,14 +55,42 @@ client.on("message", async (message) => {
         if (message.content.startsWith('support-settings')) {
             return await fancySettings(message, guildSettings);
         }
-        if (message.content.startsWith('support-close') && message.channel.parentID === guildSettings.categoryId && message.channel.id !== guildSettings.channelId) {
+        if (message.content.startsWith('support-close') && isTicketChannel(message.channel, guildSettings)) {
             return await closeTicketCmd(message, guildSettings);
+        }
+        if (message.content.startsWith('support-add') && isTicketChannel(message.channel, guildSettings)) {
+            return await addCommand(message, guildSettings);
+        }
+        if (message.content.startsWith('support-help')) {
+            return await helpCommand(message, guildSettings);
         }
 
     } catch {
 
     }
 });
+
+async function helpCommand(message, guildSettings) {
+    const commands = new Discord.MessageEmbed()
+        .setColor('#FF0000')
+        .setTitle('Commands for Support Bot')
+        .setURL('https://github.com/burturt/discord-support-bot')
+        .addFields(
+            { name: 'support-setup @role', value: 'Admin only; sets up a support channel and allows @role to close tickets as well as admins'},
+            { name: 'support-reset', value: 'Admin only; clears the setup to allow changing settings. If the same category is used when setting up again, no ticket data is lost.'},
+            { name: 'support-settings', value: 'Print out current server setup information. Useful for debugging.'},
+            { name: 'support-help', value: 'Show this help message'},
+            { name: 'support-add @user1 @user2', value: 'Add users to a ticket. Must run in ticket channel, may contain any number of user mentions OR a **single** user ID.'},
+            { name: 'support-close', value: 'Close a ticket. Can only be run by ticket author, admins, and support role.'}
+        )
+        .setFooter(`Requested by ${message.author.tag}`);
+    return await message.channel.send(commands);
+
+}
+
+function isTicketChannel(channel, guildSettings) {
+    return channel.parentID === guildSettings.categoryId && channel.id !== guildSettings.channelId;
+}
 
 async function closeTicketCmd(message, guildSettings) {
     return closeTicket(message.member, message.channel, message.guild, guildSettings);
@@ -76,15 +104,16 @@ async function closeTicket(member, channel, guild, guildSettings) {
         if (channel.topic.startsWith('CLOSING')) {
             return;
         }
+        const ticketUserId = channel.topic;
         await channel.edit({
             topic: `CLOSING ${channel.topic}`
         });
         var log = await createChatlog(channel);
-        log += `Ticket closed by ${member.user.username}#${member.user.discriminator} (${member.id})`
+        log += `Ticket closed by ${member.user.tag} (${member.id})`
         var attachment = new Discord.MessageAttachment(Buffer.from(log, 'utf-8'), `transcript-${channel.name}-${new Date().toISOString().slice(0,10)}.txt`);
         await guild.channels.cache.get(guildSettings.transcriptChannel).send(attachment);
         try {
-            await client.users.cache.get(channel.topic).send(attachment);
+            await client.users.cache.get(ticketUserId).send(attachment);
         } catch { };
 
         await channel.send('Transcript generated; closing ticket in 10 seconds');
@@ -93,6 +122,8 @@ async function closeTicket(member, channel, guild, guildSettings) {
             await channel.delete();
             return;
         }, 10000);
+    } else {
+        return await channel.send(`<@${member.id}> You do not have permission to close this ticket.`);
     }
 }
 
@@ -121,7 +152,7 @@ async function createChatlog(channel) {
 
     messagesArr.forEach( message => {
         if (message.content != null) {
-            formattedChatlog += message.createdAt.toLocaleString() + ' ' + message.author.username + '#' + message.author.discriminator + ' (' + message.author.id + '): ' + message.cleanContent + '\n';
+            formattedChatlog += message.createdAt.toLocaleString() + ' ' + message.author.tag + ' (' + message.author.id + '): ' + message.cleanContent + '\n';
         }
     });
     return formattedChatlog;
@@ -165,6 +196,9 @@ ${message.cleanContent.substr(0, 1990)}
 };
 
 async function fancySettings(message, guildSettings) {
+    if (guildSettings == null) {
+        return await message.reply('Server is not set up.');
+    }
     const embed = new Discord.MessageEmbed()
         .setColor('#FF0000')
         .setTitle(`Settings for Server ${message.guild.name}`)
@@ -216,6 +250,42 @@ async function resetGuild(message, guildSettings) {
         await guildSettings.destroy();
         return await message.reply('Existing setup deleted. Run `support-setup @role` to re-setup.');
     }
+}
+
+async function addCommand(message, guildSettings) {
+    // Test for mentions and add all mentioned users
+    if (message.mentions.members.size > 0) {
+        var list = '';
+        for (var mention of message.mentions.members) {
+            try {
+                await message.channel.createOverwrite(mention[1], {
+                    VIEW_CHANNEL: true
+                });
+                list += '<@' + mention[1].id + '> '
+            } catch {
+
+            }
+
+        }
+        return await message.reply(`Successfully added ${list}to the ticket`);
+    }
+    // Test for user named being a user id after getting arguments
+    let argument = message.content.split(/ (.+)/)[1];
+    if (argument == null) {
+        return await message.reply("Usage: `support-add USER` where USER can be multiple @mention or a single user id");
+    }
+
+    // Try converting and using userid
+    try {
+        var user = await message.guild.members.cache.get(argument.trim());
+        await message.channel.createOverwrite(user, {
+            VIEW_CHANNEL: true
+        });
+        return await message.reply(`Successfully added ${user} to the ticket`);
+    } catch {
+        return await message.reply(`Invalid user. Usage: \`support-add USER\` where USER can be multiple @mention or a single user id`);
+    }
+
 }
 
 client.login(process.env.BOT_TOKEN);
